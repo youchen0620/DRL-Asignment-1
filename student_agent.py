2b644bdbcb245acc7be37f32a10884dcee22957e
youchen0620 <ouchen916@gmail.com>
2025-03-14 14:49:06 +0800

    Fix severe inference bug

--- student_agent.py
+++ student_agent.py
@@ -15,52 +15,75 @@ def get_state(obs, target_pos, has_picked_up):
     obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look = obs[10], obs[11], obs[12], obs[13], obs[14], obs[15]
     return (agent_pos, target_pos, obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look, has_picked_up)
 
+def calculate_manhattan_distance(pos1, pos2):
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
 initialized = False
-current_state = None
+previous_state = None
+previous_action = None
 
 def get_action(obs):
     global initialized
-    global current_state
+    global previous_state
+    global previous_action
+
+    stations = [(obs[2], obs[3]), (obs[4], obs[5]), (obs[6], obs[7]), (obs[8], obs[9])]
 
     if not initialized:
         initialized = True
-        current_state = get_state(obs, (obs[2], obs[3]), False)
+        state = get_state(obs, stations[0], False)
+
+        action = None
+        if state not in q_table:
+            action = random.choice(range(6))
+        else:
+            action = int(np.argmax(q_table[state]))
+        
+        previous_state = state
+
+        return action
     
-    state = current_state
+    state = get_state(obs, previous_state[1], previous_state[8])
+
+    if state[7] and not state[8]:
+        state = get_state(obs, stations[(stations.index(state[1]) + 1) % 4], False)
+    if previous_state[0] == state[0] and state[0] == state[1]:
+        if state[6] and not state[8] and previous_action == 4:
+            state = get_state(obs, stations[(stations.index(state[1]) + 1) % 4], True)
+    if not state[7] and state[0] not in stations and state[8] and previous_action == 5:
+        state = get_state(obs, state[1], False)
 
     action = None
     if state not in q_table:
         action = random.choice(range(6))
     else:
         action = int(np.argmax(q_table[state]))
-    
-    next_state = get_state(obs, state[1], state[8])
 
-    stations = [(obs[2], obs[3]), (obs[4], obs[5]), (obs[6], obs[7]), (obs[8], obs[9])]
-
-    if next_state[7] and not next_state[8]:
-        next_state = get_state(obs, stations[(stations.index(next_state[1]) + 1) % 4], False)  
-    if state[0] == next_state[0] and state[0] in stations and next_state[0] in stations:
-        if next_state[6] and not state[8]:
-            if action == 4:
-                next_state = get_state(obs, stations[(stations.index(next_state[1]) + 1) % 4], True)
-            else:
-                next_state = get_state(obs, next_state[1], False)
-    if not next_state[7] and next_state[0] not in stations and next_state[8] and action == 5:
-        next_state = get_state(obs, next_state[1], False)
-    
-    current_state = next_state
+    previous_state = state
+    previous_action = action
 
     return action
 
-def train_agent(env, episodes=1000000, alpha=0.001, gamma=0.99, epsilon_start=1.0, epsilon_end=0.0, decay_rate=0.999995):
+def train_agent(env, episodes=1000000, alpha=0.001, gamma=0.99, epsilon_start=1.0, epsilon_end=0.0, decay_rate=0.99999):
     global q_table
     rewards_per_episode = []
     steps_per_episode = []
     epsilon = epsilon_start
+
     for ep in range(episodes):
         obs, _ = env.reset()
-        state = get_state(obs, (obs[2], obs[3]), False)
+
+        stations = [(obs[2], obs[3]), (obs[4], obs[5]), (obs[6], obs[7]), (obs[8], obs[9])]
+        target_pos = stations[0]
+
+        for i in range(1, len(stations)):
+            prev_dist_to_target = calculate_manhattan_distance((obs[0], obs[1]), target_pos)
+            curr_dist_to_target = calculate_manhattan_distance((obs[0], obs[1]), stations[i])
+
+            if curr_dist_to_target < prev_dist_to_target:
+                target_pos = stations[i]
+
+        state = get_state(obs, target_pos, False)
         done = False
         total_reward = 0
         episode_step = 0
@@ -78,25 +101,36 @@ def train_agent(env, episodes=1000000, alpha=0.001, gamma=0.99, epsilon_start=1.
             next_state = get_state(obs, state[1], state[8])
             episode_step += 1
 
-            stations = [(obs[2], obs[3]), (obs[4], obs[5]), (obs[6], obs[7]), (obs[8], obs[9])]
-
             shaped_reward = 0
             if done:
-                shaped_reward += 50
-            if next_state[7] and not next_state[8]:
-                next_state = get_state(obs, stations[(stations.index(next_state[1]) + 1) % 4], False)  
-            if state[0] != next_state[0] and next_state[0] in stations and (next_state[6] or (next_state[7] and next_state[8])):
+                shaped_reward += 100
+
+            prev_dist_to_target = calculate_manhattan_distance(state[0], state[1])
+            curr_dist_to_target = calculate_manhattan_distance(next_state[0], next_state[1])
+
+            if curr_dist_to_target < prev_dist_to_target:
                 shaped_reward += 0.2
-            if state[0] == next_state[0] and state[0] in stations and next_state[0] in stations:
-                if next_state[6] and not state[8]:
-                    if action == 4:
-                        shaped_reward += 10
-                        next_state = get_state(obs, stations[(stations.index(next_state[1]) + 1) % 4], True)
-                    else:
-                        next_state = get_state(obs, next_state[1], False)
-            if not next_state[7] and next_state[0] not in stations and next_state[8] and action == 5:
-                shaped_reward -= 100
-                episode_step = 50000
+            if state[0] == next_state[0] and action in [0, 1, 2, 3]:
+                shaped_reward -= 10
+            if state[0] == next_state[0] and ((action == 4 and not state[6] and state[0] != state[1]) or (action == 5 and not state[7] and state[0] != state[1])):
+                shaped_reward -= 10000
+            if next_state[7] and not next_state[8]:
+                next_state = get_state(obs, stations[(stations.index(next_state[1]) + 1) % 4], False)
+            if state[0] != next_state[0] and ((not state[6] and next_state[6] and not next_state[8]) or (not state[7] and next_state[7] and next_state[8])):
+                shaped_reward += 10
+            if state[0] != next_state[0] and ((state[6] and not next_state[6] and not next_state[8]) or (state[7] and not next_state[7] and next_state[8])):
+                shaped_reward -= 10
+            if state[0] != next_state[0] and next_state[0] == next_state[1] and ((next_state[6] and not next_state[8]) or (next_state[7] and next_state[8])):
+                shaped_reward += 10
+            if state[0] != next_state[0] and state[0] == state[1]:
+                shaped_reward -= 10 
+            if state[0] == next_state[0] and state[0] == state[1]:
+                if state[6] and not state[8] and action == 4:
+                    shaped_reward += 100
+                    next_state = get_state(obs, stations[(stations.index(next_state[1]) + 1) % 4], True)
+            if not next_state[7] and next_state[0] != next_state[1] and next_state[8] and action == 5:
+                shaped_reward -= 10000
+                episode_step = 5000
                 done = True
 
             reward += shaped_reward
